e8ccc888cc890ac5bfd7daa9c99248ff8d6f6f40
Gabriele Onorato <onorato.1979543@studenti.uniroma1.it>
2024-12-05 11:31:42 +0100

    hyperparameters kNN

--- AE/kNN.py
+++ AE/kNN.py
@@ -1,6 +1,7 @@
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 import pandas as pd
@@ -9,7 +10,7 @@ sys.path.append("../")
 from metrics_plotter import generate_classification_report, generate_confusion_matrix
 
 if __name__ == "__main__":
-    latent_dim = 32   # Replace with the desired latent dimension L
+    latent_dim = 8   # Replace with the desired latent dimension L
     num_classes = 5  # Number of classes in the dataset
 
     train_path = f"../dataset/train_AE_{latent_dim}/encoded_latents.csv"
@@ -28,15 +29,16 @@ if __name__ == "__main__":
     X_test = test[:, :-1]
 
     # Apply standard scaling
-    scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
-
-    knn = KNeighborsClassifier(n_neighbors=1)
-    knn.fit(X_train, y_train)
+    pipe = make_pipeline(StandardScaler(), KNeighborsClassifier())
+    param_grid = {'kneighborsclassifier__n_neighbors': [1, 3, 5, 7, 9, 11]}
+    grid = GridSearchCV(pipe, param_grid, cv=5, scoring='f1_weighted', verbose=3)
+    grid.fit(X_train, y_train)
+    best_knn = grid.best_estimator_
+    print("Best Parameters from GridSearchCV:", grid.best_params_)
+    k = grid.best_params_['kneighborsclassifier__n_neighbors']
 
     # Test on the test set
-    y_pred = knn.predict(X_test)
+    y_pred = best_knn.predict(X_test)
     print("\nClassification Report on Test Data:\n")
     print(classification_report(y_test, y_pred))
     class_report = classification_report(y_test, y_pred, output_dict=True)
