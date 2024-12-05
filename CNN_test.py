2cfa4724609add1f779f621963c5bbd63d91dbd5
Gabriele Onorato <onorato.1979543@studenti.uniroma1.it>
2024-12-05 19:15:47 +0100

    overfit vs not

---
+++ CNN_test.py
@@ -0,0 +1,59 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import confusion_matrix, classification_report
+import matplotlib.pyplot as plt
+import os
+import sys
+from metrics_plotter import generate_classification_report, generate_confusion_matrix
+
+if __name__ == "__main__":
+    # Parameters
+    input_size = (96, 96)
+    batch_size = 32
+    test_dir = "dataset/test"
+    model_path = "cnn_model.keras"
+    output_dir = "metrics_plot"
+    boosting = False
+
+    if boosting:
+        model_path = "cnn_model_boosting.keras"
+        
+
+    # Ensure output directory exists
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Load the model
+    model = tf.keras.models.load_model(model_path)
+
+    # Prepare the test data generator
+    test_datagen = ImageDataGenerator(rescale=1.0 / 255)
+    test_generator = test_datagen.flow_from_directory(
+        test_dir,
+        target_size=input_size,
+        batch_size=batch_size,
+        class_mode="categorical",
+        shuffle=False
+    )
+
+    # Get true labels and class indices
+    true_labels = test_generator.classes
+    class_indices = test_generator.class_indices
+    class_names = list(class_indices.keys())
+
+    # Predict on the test set
+    predictions = model.predict(test_generator)
+    predicted_labels = np.argmax(predictions, axis=1)
+
+    # Confusion Matrix
+    conf_matrix = confusion_matrix(true_labels, predicted_labels)
+    generate_confusion_matrix(conf_matrix, class_names, output_dir, "conf_matrix_CNN.png")
+
+    # Classification Report
+    class_report = classification_report(
+        true_labels, predicted_labels, target_names=class_names, output_dict=True
+    )
+    print("Classification Report:\n")
+    print(classification_report(true_labels, predicted_labels, target_names=class_names))
+    generate_classification_report(class_report, output_dir, "class_report_CNN.png")
+    sys.exit(0)
---
+++ CNN_train.py
@@ -0,0 +1,127 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import backend as K
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.callbacks import Callback
+from tensorflow.keras.callbacks import EarlyStopping
+import matplotlib.pyplot as plt
+import sys
+import numpy as np
+
+if __name__ == "__main__":
+
+    # Parameters
+
+    input_size = (96, 96)
+    batch_size = 64
+    num_classes = 5
+    epochs = 15
+
+    boosting = False
+    focal = True
+
+    train_dir = "dataset/train"
+    test_dir = "dataset/test"
+
+    loss_fn = "categorical_crossentropy"
+ 
+    if boosting:
+        train_dir = "dataset/train_boosted"
+
+    # Data Augmentation and Preprocessing
+    train_datagen = ImageDataGenerator(rescale=1.0 / 255)
+
+    test_datagen = ImageDataGenerator(rescale=1.0 / 255)
+
+    train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=input_size,
+        batch_size=batch_size,
+        class_mode="categorical"
+    )
+
+    test_generator = test_datagen.flow_from_directory(
+        test_dir,
+        target_size=input_size,
+        batch_size=batch_size,
+        class_mode="categorical"
+    )
+
+    _lambda = 10e-5 # L2 Regularization
+    # Build the CNN Model with L2 regularization and more dense layers
+    model = Sequential([
+        Conv2D(64, (3, 3), activation="relu", input_shape=(96, 96, 3), kernel_regularizer=l2(_lambda)),
+        BatchNormalization(),
+        MaxPooling2D((2, 2)),
+        Conv2D(128, (3, 3), activation="relu", kernel_regularizer=l2(_lambda)),
+        BatchNormalization(),
+        MaxPooling2D((2, 2)),
+        Conv2D(256, (3, 3), activation="relu", kernel_regularizer=l2(_lambda)),
+        BatchNormalization(),
+        MaxPooling2D((2, 2)),
+        Conv2D(512, (3, 3), activation="relu", kernel_regularizer=l2(_lambda)),
+        BatchNormalization(),
+        MaxPooling2D((2, 2)),
+        Flatten(),
+        Dropout(0.1),
+        Dense(128, activation="relu", kernel_regularizer=l2(_lambda)),
+        Dropout(0.1),
+        Dense(128, activation="relu", kernel_regularizer=l2(_lambda)),
+        Dropout(0.1),
+        Dense(num_classes, activation="softmax")
+    ])
+
+
+    model.compile(
+        optimizer="adam",
+        loss=loss_fn,
+        metrics=["f1_score"],
+    )
+
+    # Model Summary
+    model.summary()
+
+    # Train the Model
+    history = model.fit(
+        train_generator,
+        epochs=epochs,
+        validation_data=test_generator
+    )
+
+    # Save the Model
+    if boosting:
+        model.save("cnn_model_boosting.keras")
+    else:
+        model.save("cnn_model.keras")
+    if focal:
+        model.save("cnn_model_focal.keras")
+
+    # Plot and save Loss and Accuracy
+    plt.figure()
+    plt.plot(history.history['loss'], label='Training Loss')
+    plt.plot(history.history['val_loss'], label='Validation Loss')
+    plt.title('Loss Over Epochs')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig('loss_plot.png')
+
+
+    # Plot and save F1 Score if available
+    f1_scores = history.history['f1_score']
+    val_f1_scores = history.history['val_f1_score']
+    f1_scores = np.mean(f1_scores, axis=1)
+    val_f1_scores = np.mean(val_f1_scores, axis=1)
+    plt.figure()
+    plt.plot(f1_scores, label='F1 Score')
+    plt.plot(val_f1_scores, label='Validation F1 Score')
+    plt.title('F1 Score Over Epochs')
+    plt.xlabel('Epochs')
+    plt.ylabel('F1 Score')
+    plt.legend()
+    plt.savefig('f1_score_plot.png')
+
+    sys.exit(0)
